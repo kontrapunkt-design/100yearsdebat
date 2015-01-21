066a88ea7f98eab83399dbf8e0eeaf7716e5e241
madsviktor <madsviktor@gmail.com>
2015-01-21 15:29:41 +0100

    new version

--- public/js/libs/jquery.cloudinary.js
+++ public/js/libs/jquery.cloudinary.js
@@ -694,6 +694,8 @@
 
     if (initializing) {
       this.bind("fileuploaddone", function(e, data) {
+        console.log('here');
+        console.log(data);
         if (data.result.error) return;
         data.result.path = ["v", data.result.version, "/", data.result.public_id,
                             data.result.format ? "." + data.result.format : ""].join("");
